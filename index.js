79df535a4f9c511e6c4593cc63af37322419e43f
Ian Gloude <igloude@gmail.com>
2020-02-15 18:29:49 -0600

    progress on output

--- index.js
+++ index.js
@@ -1,28 +1,60 @@
-const chalk = require("chalk"),
+var chalk = require("chalk"),
   clear = require("clear"),
   figlet = require("figlet"),
   fs = require("fs"),
   prompt = require("prompt"),
   yaml = require("js-yaml");
 
-function evaluateChecklistName() {}
+var art;
 
-function getChecklist() {
+figlet("Checklist", function(err, fig) {
+  if (err) {
+    console.log("Something went wrong with figlet...");
+    console.dir(err);
+    return;
+  }
+  art = fig;
+});
+
+function promptChecklistName() {
+  prompt.get(["checklist"], function(err, result) {
+    var list = result.checklist;
+
+    console.log("result.checklist:");
+    console.log(list);
+
+    if (list != null) {
+      getChecklist("./__mocks__/data.yaml");
+    } else {
+      restart();
+    }
+  });
+}
+
+function getChecklist(file) {
   try {
-    let fileContents = fs.readFileSync("./__mocks__/data.yaml", "utf8");
+    let fileContents = fs.readFileSync(file, "utf8");
     let data = yaml.safeLoadAll(fileContents);
 
-    console.log(data);
+    printer(data);
   } catch (e) {
     console.log(e);
+    restart();
   }
 }
 
 function restart() {
   console.log("Please enter a valid checklist name.");
   console.log("");
-  evaluateChecklistName();
+  promptChecklistName();
+}
+
+function printer(data) {
+  clear();
+  console.log(art);
+  console.log(chalk.inverse(data[1].title));
+  console.log(data);
 }
 
 prompt.start();
-evaluateChecklistName();
+promptChecklistName();
